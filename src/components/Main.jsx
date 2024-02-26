58c74820003363f8da1fd00f4df3c9cd32dd65fa
user <user@XVIII-DEMS.local>
2024-02-26 07:29:10 +0100

    finished the bookmenow section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -69,8 +69,8 @@ const Main = () => {
           />
         </div>
 
-        <div className="bg-[#3D5654] rounded-3xl p-5 right-main">
-          <div className="flex justify-between">
+        <div className=" rounded-3xl bg-[#3D5654] right-main ">
+          <div className="flex rounded-t-3xl justify-between p-5 bg-[#415C5A]">
             <div className="text-white">
               <p className="text-[10px]">FROM</p>
               <p className="font-normal text-lg">JFK</p>
@@ -83,10 +83,22 @@ const Main = () => {
               <p className="font-normal text-lg">BOM</p>
             </div>
           </div>
-          <div>
-            <Image src="/images/main.png" width={150} height={150} alt="main" />
+          <div className="py-9 bg-[#415C5A] object-fill w-full">
+            <Image src="/images/main.png" width={350} height={350} alt="main" />
           </div>
-          <div></div>
+          <div className=" h-max bg-[#3D5654] p-2">
+            <div className="flex justify-between text-white">
+              <p className="text-[12px] px-4 py-2 cursor-pointer hover:bg-[#D3A019] hover:rounded-3xl ">
+                NON STOP
+              </p>
+              <p className="text-[12px] px-4 py-2 cursor-pointer hover:bg-[#D3A019] hover:rounded-3xl ">
+                ONE STOP
+              </p>
+              <p className="text-[12px] px-4 py-2 cursor-pointer hover:bg-[#D3A019] hover:rounded-3xl ">
+                MORE STOP
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
